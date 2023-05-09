cc42a83e69e52d50dd241d479cd303ec81774d00
hakimemma <113105266+hakimemma@users.noreply.github.com>
2023-05-09 11:56:12 +0100

    FINISHING UP BUGS PT 1

--- Assets/Scenes/scripts/destroy.cs
+++ Assets/Scenes/scripts/destroy.cs
@@ -14,7 +14,7 @@ public class destroy : MonoBehaviour
     {
         if(other.tag=="Player")
         {
-            Destroy(cube,7f);
+            Destroy(cube,5f);
             Destroy(gameObject);
         }
     }
