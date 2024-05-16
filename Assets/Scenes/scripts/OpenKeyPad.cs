fbed490cf366350c2236ea574b9919094eb55917
hakimemma <113105266+hakimemma@users.noreply.github.com>
2024-05-16 19:46:41 +0100

    key pad pt2

---
+++ Assets/Scenes/scripts/OpenKeyPad.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenKeyPad : MonoBehaviour
+{
+    public GameObject keyPadObjv;
+    public GameObject keyPadText;
+
+    public bool inReach;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inReach = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = true;
+            keyPadText.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Reach")
+        {
+            inReach = false;
+            keyPadText.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact") && inReach)
+        {
+            keyPadObjv.SetActive(true);
+        }
+    }
+}
